ac67fba64baa1c9347f8aa1c4880ecd926e3e784
Richard Gill <richard@rgill.co.uk>
2019-12-02 19:57:24 +0000

    first star

---
+++ lib/mix/tasks/day1.ex
@@ -0,0 +1,15 @@
+defmodule Mix.Tasks.AOC.Day1 do
+  use Mix.Task
+
+  def fuelForMass(mass) do
+    Integer.floor_div(mass, 3) - 2
+  end
+
+  def run(_) do
+    massesAsStrings = String.split(String.trim(File.read!("./lib/mix/tasks/input1.txt")), "\n")
+    massesAsNumbers = Enum.map(massesAsStrings, fn x -> elem(Integer.parse(x), 0) end)
+    Enum.map(massesAsNumbers, &fuelForMass/1)
+    answer = Enum.sum(Enum.map(massesAsNumbers, &fuelForMass/1))
+    IO.puts("Answer: #{answer}")
+  end
+end
